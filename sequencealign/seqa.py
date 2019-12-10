089f419395d709d5ea752d4caec8cc2b9d143874
J.Williamn <hwilliamnq@gmail.com>
2019-12-10 21:12:10 +0300

    local alignment - still some wrong answers and memory limits

---
+++ sequencealign/seqa.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# Homework Assignment
+# DP - Local sequence alignment 
+
+class LocalAlign():
+    def __init__(self, match, mismatch, indel, th, m, n):
+        self.match = match 
+        self.mismatch = mismatch
+        self.indel = indel
+        self.th = th
+
+        self.m = m
+        self.n = n
+        #self.sequence = [0]*(self.n+1)
+
+        self.result = []
+
+    def align(self, p, s):
+        score = [0]*(self.m+1)
+        patt = [0]*(self.m+1)
+
+        # Compute score alignment for all position of sequence
+        for j in range(1, self.n + 1):
+            score[0] = 0
+            for i in range(1, self.m + 1):
+                # print('s and p', s[j-1], p[i -1])
+                # print('patt:', patt)
+                # print('i-1, j-1:', patt[i-1] + (self.match if s[j-1] == p[i-1] else self.mismatch))
+                # print('i, j-1:', patt[i] + self.indel)
+                # print('i-1, j:', score[i - 1] + self.indel)
+
+                score[i] = max(patt[i-1] + (self.match if s[j-1] == p[i-1] else self.mismatch),
+                            patt[i] + self.indel,
+                            score[i - 1] + self.indel)
+                
+
+                #print('score', i, ':', score[i])
+                if i == self.m:
+                    #print('last:',i)
+                    if score[i] >= self.th:
+                        #self.result[j] = score[i]
+                        self.result.append((j,score[i]))
+            
+                    #print('prev score',patt)
+                    patt = score.copy()
+                    
+        
+        return self.result
+
+
+ma, mi, d, th = map(int, input().split())
+m, n = map(int, input().split())
+p = [char for char in input()]
+s = [char for char in input()]
+
+
+approxMatching = LocalAlign(ma, mi, d, th, m, n)
+res = approxMatching.align(p, s)
+
+#print(res)
+for l in res:
+    print(l[0], l[1])
